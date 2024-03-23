bf789ff4039308b506e32d9f59729b699c258002
herc <herculeshorse@tuta.io>
2024-03-23 14:14:51 +0200

    remove action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
 import cloudflare from "@astrojs/cloudflare";
+
 
 // https://astro.build/config
 export default defineConfig({
